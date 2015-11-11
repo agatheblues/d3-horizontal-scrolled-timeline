5f5fbd8abf1cc0c01f342856fed3abdba2146a5a
HippieDeMerde <lenclen.agathe@gmail.com>
2015-11-11 20:50:16 +0100

    Refactor + Comments and structure

--- Timeline/js/dataset.js
+++ Timeline/js/dataset.js
@@ -194,5 +194,54 @@ var dataset = [
 	        }
 		  ]
 	    },
+	],
+	//Dataset2
+	[
+		{
+		 "section": "persoXP",
+		 "events": [
+		    {
+	            "debutDate": 2011,
+	            "endDate": 2014,
+	            "name": "Test 4",
+	            "description": "One pie a day"
+	        },
+		    {
+	            "debutDate": 2012,
+	            "endDate": 2013,
+	            "name": "President of the pie cooking club",
+	            "description": "Student association"
+	        },
+		    {
+	            "debutDate": 2013,
+	            "endDate": 2014,
+	            "name": "The longest pie ever",
+	            "description": "Specialisation Pie Project"
+	        }
+		  ]
+	    },
+	    {
+		 "section": "proXP",
+		 "events": [
+		    {
+	            "debutDate": 2012,
+	            "endDate": 2012,
+	            "name": "Internship at La bonne auberge",
+	            "description": "Restaurant hosting"
+	        },
+		    {
+	            "debutDate": 2013,
+	            "endDate": 2013,
+	            "name": "Internship at La bonne tarte",
+	            "description": "Responsible of the croissants"
+	        },
+		    {
+	            "debutDate": 2014,
+	            "endDate": 2014,
+	            "name": "Internship at Le bon gateau",
+	            "description": "The perfect pie"
+	        }
+		  ]
+	    },
 	]
 ];
--- Timeline/js/timeline.js
+++ Timeline/js/timeline.js
@@ -1,15 +1,14 @@
-//Width and height
-var gapYears = 150; //Space between two years in px
 var w = 1000;
-var h = 450; //TODO Replace 4 per number of events in dataset
-var marginAroundTimeline = 80;
-var tickPadding = 15;
-var betweenEventPadding = 25;
-var eventLeftPadding = 5;
-var lineTopPadding = 6;
-var maxHeight = 100; //event text maximum height
-var nameTopPadding = 6;
-//Create SVG element
+var h = 450;
+var marginAroundTimeline = 80; 	// Margin between the axis and the events
+var tickPadding = 15; 			// Padding between ticks and dates on the axis
+var betweenEventPadding = 25;	// Space between events horizontally
+var eventLeftPadding = 5;		// Padding between the vertical line and the event texts
+var linePadding = 2;			// Space between the top of the line and the top of the event texts (bottom for events at the bottom)
+var maxHeight = 100; 			// Events maximum height
+var nameTopPadding = 6;			// For event names at the top part of the timeline, padding to avoid overlap between the name and description
+
+/* Create SVG element */
 var svg = d3.select('#chart')
 					.append("svg")
 					.attr("width", w)
@@ -17,16 +16,27 @@ var svg = d3.select('#chart')
 						.append('g')
 						.attr('id','mainTimeline');
 
-/**** Vertical timeline ****/
+/* Timeline Container */
 var timeline = d3.select('g#mainTimeline').append('g')
 											.attr("id","timeline");
-//Create Y axis
+
+/* Axis container */
 var yAxis = timeline.append("g")
 						.attr("class", "y axis")
 						.attr("transform", "translate("+ marginAroundTimeline + ","+ h/2 +")");
 
+/* Update pattern to go through several datasets */
 function update(dataset){
-	//Calculate min and max year for a given section
+	/* Section names */
+	/** We need to store section names so we can compare g.section index name and get the corresponding index position
+	Needed in getSectionIndex **/
+	var sectionNames = [];
+	dataset.forEach(function(item){
+		sectionNames.push(item.section);
+	});
+
+	/* Axis Creation */
+	/** For a given dataset, find minimum debut year and maximum end year over the events **/
 	var minYear = d3.min(dataset, function(d){
 		return d3.min(d.events,function(k){return k.debutDate;});
 	});
@@ -34,51 +44,35 @@ function update(dataset){
 		return d3.max(d.events,function(k){return k.endDate;});
 	});
 
-	//Store all the dates for a given section
-	var timelineDates = [];
-	var countDates = minYear;
-	while (countDates  <= maxYear+1) {
-		timelineDates.push(countDates);
-		countDates++;
-	}
-
-	var eventWidth = w/timelineDates.length-betweenEventPadding; //equivalent of yScale.rangeBand (that is equal to 0 here so cannot use)
-	var nameTopArray = []; //Store name height at the bottom
-	var descriptionBottomArray = []; //Store description height at the top
-	var nameBottomArray = []; //Store name height at the bottom
-	var descriptionTopArray = []; //Store description height at the top
-	var sectionNames = []; //Store section names
-	dataset.forEach(function(item){
-		sectionNames.push(item.section);
-	});
-
-
-	//scale
+	/** Scale definition **/
 	var yScale = d3.scale.ordinal()
-						 .domain(timelineDates)
-						 .rangeRoundBands([0,w-2*marginAroundTimeline],1,0);
+						 .domain(d3.range(minYear,maxYear+2))					// d3.range doesnt include the stop value so +1. If an event begins on the maxYear, it needs space to be displayed so +1. -> +2
+						 .rangeRoundBands([0,w-2*marginAroundTimeline],1,0);	// 1 so the first tick begins on the left edge of the axis
 
-	//Define Y axis
+	/** Axis definition **/
 	var yAxisDef = d3.svg.axis()
 					  .scale(yScale)
 					  .orient("bottom")
-					  .tickSize(0) 					// No tick visible
-					  .tickPadding(tickPadding)				// Padding for the label
-					  .tickFormat(d3.format("d"));	// Remove comma for thousands (2,010)
+					  .tickSize(0) 						// No tick visible
+					  .tickPadding(tickPadding)			// Padding for the label
+					  .tickFormat(d3.format("d"));		// Remove comma for thousands (2,010)
 
-
+	/** Axis creation and animation **/
 	yAxis.transition()
 		.duration(1000)
 		.call(yAxisDef);
 
+	/** Axis ticks to be circles **/
 	var ticks = yAxis.selectAll(".tick");
 		ticks.each(function() {
-			d3.select(this).append("circle").attr("r", 7);
+			d3.select(this).append("circle").attr("r", 5);
 		});
+	/* End of axis creation */
 
 
-	/**** Section container ****/
-
+	/* Section containers */
+	/** Binded data : sections names
+	Create one <g> per section **/
 	var sectionContainer = d3.select('g#mainTimeline').selectAll('g.section')
 											.data(dataset);
 
@@ -91,7 +85,10 @@ function update(dataset){
 				sectionContainer.exit()
 								.remove();
 
-	/**** Events container ****/
+	/*  Events containers */
+	/** Binded data : events
+	Create one <g> per event, nested under the corresponding section **/
+	var eventWidth = w/d3.range(minYear,maxYear+2).length-betweenEventPadding; // Allocated width between events (equivalent of rangeBand, which we can't use here as it is null because rangeRoundBands padding is = 1)
 
 	var eventsContainer = d3.select('g#mainTimeline').selectAll('g.section').selectAll('g.events')
 														.data(function(d){return d.events;});
@@ -101,59 +98,64 @@ function update(dataset){
 														.attr('class','events')
 														.attr('width',eventWidth);
 
-										eventsContainer.attr('id',function(d,i){return i;})
-														.attr('text-anchor',function(d){
-															if (d.debutDate === minYear) {
-																return 'start';
-															}
-														});
+										eventsContainer.attr('id',function(d,i){return i;});
 
 										eventsContainer.exit()
 														.remove();
 
 
+	/* Events names */
+	/** Binded data : events.name
+	Create one <text> per events name, nested under the corresponding event **/
+	var namesTopArray = []; 		// Store names height, from events at the top of timeline
+	var namesBottomArray = []; 		// Store names height, from events at the bottom of timeline
 
-
-/*** Add events names ***/
 	var names = eventsContainer.selectAll('text.name')
-											.data(function(d){return [d.name];});
+											.data(function(d){return [d.name];}); // [] otherwise it's one data per character
 
 						names.enter()
 					 				.append('text')
 									.attr('class','name');
 
 						names.text(function(d){return d;})
-								.attr('opacity',0)
+								.each(function(d,i){
+									var currentName = d3.select(this);						//The current <text> element
+									textWrap(currentName,eventWidth);						// Wrap text in dedicated width
+									var nameHeight = textHeight(currentName);				// Calculate height of the whole <text>
+
+									var index = getSectionIndex(this.parentNode,sectionNames); 	// Get the parent section index to know if the event is in the top part or the bottom part of the timeline
+										/*** If the section index is even, the events are on the top part of the timeline.
+											Because of the wrap, names and descriptions overlap.
+											Names at the top should be translated up, from their own height.
+											No need to move names at the bottom. ***/
+										if (index % 2 === 0) {
+											currentName.attr('transform','translate(0,'+ -(nameHeight + nameTopPadding) +')');
+											namesTopArray.push(nameHeight);		// Store name height for later
+										} else {
+											namesBottomArray.push(nameHeight);	// Store name height for later
+										}
+								});
+
+						/*** Names animation - Initial to end state ***/
+						names.attr('opacity',0)
 								.attr('y',function(){
-									var index = getSectionIndex(this.parentNode,sectionNames); //Section index
-									return index % 2 === 0 ? marginAroundTimeline : -marginAroundTimeline;
+									var index = getSectionIndex(this.parentNode,sectionNames); 				//Section index
+									return index % 2 === 0 ? marginAroundTimeline : -marginAroundTimeline;	// If it's a name from the top area, transition goes from the bottom to the top ; and the other way around:
 								})
 								.transition()
 								.duration(1000)
 								.attr('y',0)
-								.attr('opacity',1)
-								.each(function(d,i){
-									//this = <text>name</text>
-									//Wrap <text>
-									textWrap(d3.select(this),eventWidth);
-									//Compute name text height
-									var nameHeight = textHeight(d3.select(this));
-
-									//For events in the top (=index odd), push name position up from the event name height
-									var index = getSectionIndex(this.parentNode,sectionNames); //Section index
-									if (index % 2 === 0) {
-										d3.select(this).attr('transform','translate(0,'+ (-nameHeight-nameTopPadding) +')');
-										nameTopArray.push(nameHeight);
-									} else {
-										//Store name height for later, to push description down
-										nameBottomArray.push(nameHeight);
-									}
-								});
+								.attr('opacity',1);
 
 						names.exit()
 								.remove();
 
-/*** Add events descriptions ***/
+	/* Events descriptionss */
+	/** Binded data : events.description
+	Create one <text> per events description, nested under the corresponding event **/
+	var descriptionsTopArray = []; 			// Store descriptions height, from events at the top of timeline
+	var descriptionsBottomArray = [];		// Store descriptions height, from events at the bottom of timeline
+
 	var descriptions = eventsContainer.selectAll('text.description')
 											.data(function(d){return [d.description];});
 
@@ -163,108 +165,117 @@ function update(dataset){
 
 
 						descriptions.text(function(d){return d;})
-										.attr('opacity',0)
-										.attr('y',function(){
-											var index = getSectionIndex(this.parentNode,sectionNames); //Section index
-											return index % 2 === 0 ? (marginAroundTimeline-nameTopArray[d3.select(this.parentNode).attr('id')]) : -marginAroundTimeline;
-										})
-										.transition()
-				                        .duration(1000)
-										.attr('y',0)
-										.attr('opacity',1)
 										.each(function(d,i){
-											//Wrap <text>
-											textWrap(d3.select(this),eventWidth);
-											//Compute description text height
-											var descriptionHeight = textHeight(d3.select(this));
+											var currentDescription = d3.select(this);				//The current <text> element
+											textWrap(currentDescription,eventWidth);				// Wrap text in dedicated width
+											var descriptionHeight = textHeight(currentDescription);	// Calculate height of the whole <text>
 
-		  									//For events in the bottom (=index even), push description position down from the event name height
-		  									var index = getSectionIndex(this.parentNode,sectionNames); //Section index
-		  									if (index % 2 !== 0) {
-		  										d3.select(this).attr('transform','translate(0,'+ nameBottomArray[d3.select(this.parentNode).attr('id')] +')');
-												descriptionBottomArray.push(descriptionHeight);
-		  									} else {
-												//Store name height for later, to push description down
-												descriptionTopArray.push(descriptionHeight);
-											}
+											var index = getSectionIndex(this.parentNode,sectionNames); // Get the parent section index to know if the event is in the top part or the bottom part of the timeline
+												/*** If the section index is odd, the events are on the bottom part of the timeline.
+													Because of the wrap, names and descriptions overlap.
+													Descriptions at the bottom should be translated down, from the height of the corresponding event name.
+													No need to move names. ***/
+			  									if (index % 2 !== 0) {
+			  										currentDescription.attr('transform','translate(0,'+ namesBottomArray[d3.select(this.parentNode).attr('id')] +')');
+													descriptionsBottomArray.push(descriptionHeight);	// Store description height for later
+			  									} else {
+													descriptionsTopArray.push(descriptionHeight);		// Store description height for later
+												}
 										});
+
+						/*** Desciprtions animation - Initial to end state ***/
+						descriptions.attr('opacity',0)		// If it's a description from the top area, transition goes from the bottom to the top ; and the other way around:
+									.attr('y',function(){
+										var index = getSectionIndex(this.parentNode,sectionNames); // Get the parent section index to know if the event is in the top part or the bottom part of the timeline
+										return index % 2 === 0 ? (marginAroundTimeline-namesTopArray[d3.select(this.parentNode).attr('id')]) : -marginAroundTimeline;
+									})
+									.transition()
+									.duration(1000)
+									.attr('y',0)
+									.attr('opacity',1);
 
 						descriptions.exit()
 										.remove();
 
-	/** Translate events container to align bottom line and top line***/
+	/* Events containers position
+	Translate events container to
+	- At the top, align description bottom lines
+	- At the bottom, align name top lines
+	- On both areas, move events Container across the timeline axis to be aligned with their corresponding date */
 	eventsContainer.attr('transform',function(d){
-					//If section index is pair, goes to the left of the timeline
-					//If section index is even; goes to the right of the timeline
-					var index = getSectionIndex(this,sectionNames); //Section index
-					if (index % 2 === 0) {//top
-						//on x, align with axis tick
-						//on y, move to the top and align the description bottom horizontally by substracting the description height
-						return 'translate('+ (marginAroundTimeline+yScale(d.debutDate)) +','+ (h/2 - marginAroundTimeline - descriptionTopArray[d3.select(this).attr('id')]) +')';}
-					else {//bottom
-						//on y; align name horizontally on margin arount timeline
-						return 'translate('+ (marginAroundTimeline+yScale(d.debutDate)) +','+ (h/2 + marginAroundTimeline) +')';
-					}
+						var index = getSectionIndex(this,sectionNames); // Get the parent section index to know if the event is in the top part or the bottom part of the timeline
+							if (index % 2 === 0) {
+								/** Top
+								On x, align with axis tick
+								On y, move to the top and align the description bottom horizontally by substracting the description height **/
+								return 'translate('+ (marginAroundTimeline+yScale(d.debutDate)) +','+ (h/2 - marginAroundTimeline - descriptionsTopArray[d3.select(this).attr('id')]) +')';}
+							else {
+								/** Bottom
+								On x, align with axis tick
+								On y; align name horizontally on margin around timeline **/
+								return 'translate('+ (marginAroundTimeline+yScale(d.debutDate)) +','+ (h/2 + marginAroundTimeline) +')';
+							}
 				});
 
-	/*** Add events lines ***/
+	/* Events vertical lines */
 	var eventLines = eventsContainer.selectAll('line.eventLines')
-											.data(function(d){return [d.name];}); //d.name to have the correct amount of lines; cannot bind just d
+											.data(function(d){return [d.name];}); 	//d.name to bind with the correct amount of lines; cannot bind just d.
 
-					eventLines.enter()
-								.append('line')
-								.attr('class','eventLines')
-								.attr('x1',0)
-								.attr('x2',0);
+							eventLines.enter()
+										.append('line')
+										.attr('class','eventLines')
+										.attr('x1',0)
+										.attr('x2',0);
 
-
-				/**Lines animation initalisation**/
-				eventLines.attr('y1',function(d){ //for animation, on top part, y1 must be originally at the axis level
-									var index = getSectionIndex(this.parentNode,sectionNames); //Section index
-									if (index % 2 === 0) {//top
-										return marginAroundTimeline + descriptionTopArray[d3.select(this.parentNode).attr('id')];
-									}
+				/** Lines animation - initalisation **/
+				eventLines.attr('y1',function(d){
+									var index = getSectionIndex(this.parentNode,sectionNames); 		// If it's a line from the top area, transition goes from the bottom to the top
+										if (index % 2 === 0) {
+											return marginAroundTimeline + descriptionsTopArray[d3.select(this.parentNode).attr('id')];
+										}
 								})
-								.attr('y2',function(d){ //for animation, on bottom part, y2 must be originally at the axis level
-												var index = getSectionIndex(this.parentNode,sectionNames); //Section index
-												if (index % 2 !== 0) {//Bottom
-													return -marginAroundTimeline + tickPadding + d3.select('g.tick text').node().getBBox().height; //size of tick texts
-												}
+								.attr('y2',function(d){ // If it's a line from the bottom area, transition goes from the top to the bottom
+										var index = getSectionIndex(this.parentNode,sectionNames);
+											if (index % 2 !== 0) {//Bottom
+												return -marginAroundTimeline + tickPadding + d3.select('g.tick text').node().getBBox().height; //size of tick texts
+											}
 								});
 
+				/** Lines animation - end state **/
 				eventLines.transition()
 								.duration(1000)
 								.attr('y2',function(d){
-									var index = getSectionIndex(this.parentNode,sectionNames); //Section index
-									if (index % 2 === 0) {//top
-										return marginAroundTimeline + descriptionTopArray[d3.select(this.parentNode).attr('id')];
-									} else {//bottom
-										return nameBottomArray[d3.select(this.parentNode).attr('id')] + descriptionBottomArray[d3.select(this.parentNode).attr('id')] + lineTopPadding;
-									}
+									var index = getSectionIndex(this.parentNode,sectionNames);
+										if (index % 2 === 0) {
+											return marginAroundTimeline + descriptionsTopArray[d3.select(this.parentNode).attr('id')];
+										} else {
+											return namesBottomArray[d3.select(this.parentNode).attr('id')] + descriptionsBottomArray[d3.select(this.parentNode).attr('id')] + linePadding;
+										}
 								})
 								.attr('y1',function(d){
-												var index = getSectionIndex(this.parentNode,sectionNames); //Section index
-												if (index % 2 === 0) {//top
-													return (-nameTopArray[d3.select(this.parentNode).attr('id')]-lineTopPadding);
-												} else {//bottom
-													return -marginAroundTimeline + tickPadding + d3.select('g.tick text').node().getBBox().height; //size of tick texts
-												}
+									var index = getSectionIndex(this.parentNode,sectionNames);
+										if (index % 2 === 0) {
+											return (-namesTopArray[d3.select(this.parentNode).attr('id')]-linePadding);
+										} else {
+											return -marginAroundTimeline + tickPadding + d3.select('g.tick text').node().getBBox().height; //size of tick texts
+										}
 								});
 
 					eventLines.exit()
 								.remove();
 }
 
-
+/* First display */
 update(dataset[0]);
 
-//Count the element in a selection
+/* Count the elements in a selection */
 d3.selection.prototype.size = function() {
 var n = 0;
 this.each(function() { ++n; });
 return n;
 };
 
+/* Calculate text height, given the number of <tspan> and the line height */
 function textHeight(text){
 	//Compute description text height
 	var lineHeight = text.select('tspan').attr('dy');
@@ -273,6 +284,7 @@ function textHeight(text){
 
 }
 
+/* Wrap <text>, with d3plus */
 function textWrap(text,eventWidth){
 	//Wrap text
 	d3plus.textwrap()
@@ -284,7 +296,9 @@ function textWrap(text,eventWidth){
 	    .draw();
 }
 
+/* childNode argument must be a child node of a <g> element with a 'section' attribute (sectionContainer)
+Given this childNode, getSectionIndex find the position of the 'section' attribute (= name of the section) in the sectionnames array.
+Return this position. */
 function getSectionIndex(childNode,sectionNames){
-	//find position of the parentNode g.section section attribute in the section names. Return index which is the section index in the dataset.
 	return sectionNames.indexOf(d3.select(childNode.parentNode).datum().section);
 }
